b1bff92fc36a5307b32f0dbf94e294ae4a7b00fe
Phoenix-JSW <jswylds@gmail.com>
2022-03-15 19:32:13 +0800

    Pledge Model Complete

--- crowdfunding/projects/models.py
+++ crowdfunding/projects/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 # Create your models here.
 class Project(models.Model):
     title = models.CharField(max_length=200)
@@ -9,3 +10,13 @@ class Project(models.Model):
     is_open = models.BooleanField()
     date_created = models.DateTimeField()
     owner = models.CharField(max_length=200)
+
+class Pledge(models.Model):
+    amount = models.IntegerField()
+    comment = models.CharField(max_length=200)
+    anonymous = models.BooleanField()
+    project = models.ForeignKey(
+        'Project',on_delete=models.CASCADE,
+        related_name='pledges'
+    )
+    supporter = models.CharField(max_length=200)
