4e59d32296c14cafef6f73b1c6ffce740185e18a
Phoenix-JSW <jswylds@gmail.com>
2022-03-26 09:05:58 +0800

    Token Authentication

--- crowdfunding/projects/views.py
+++ crowdfunding/projects/views.py
@@ -3,14 +3,16 @@ from rest_framework.response import Response
 from .models import Project
 from .serializers import ProjectSerializer
 from django.http import Http404
-from rest_framework import status
+from rest_framework import status, permissions
 from .models import Project, Pledge
 from .serializers import ProjectSerializer, PledgeSerializer, ProjectDetailSerializer
-
+from .permissions import IsOwnerOrReadOnly
 
 # Create your views here.
 
 class PledgeList(APIView):
+    
+
     def get(self, request):
         pledges = Pledge.objects.all()
         serializer = PledgeSerializer(pledges, many=True)
@@ -30,6 +32,7 @@ class PledgeList(APIView):
             )
 
 class ProjectList(APIView):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
     def get(self, request):
         projects = Project.objects.all()
@@ -51,17 +54,34 @@ class ProjectList(APIView):
         )
 
 class ProjectDetail(APIView):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly,
+    IsOwnerOrReadOnly
+    ]
 
     def get_object(self, pk):
         try:
-            return Project.objects.get(pk=pk)
+            # return Project.objects.get(pk=pk)
+            project =Project.objects.get(pk=pk)
+            self.check_object_permissions(self.request,project)
+            return project
         except Project.DoesNotExist:
             raise Http404
-        
+               
     def get(self, request, pk):
         project = self.get_object(pk)
         serializer = ProjectDetailSerializer(project)
         return Response(serializer.data)
+
+    def put(self, request, pk):
+        project = self.get_object(pk)
+        data = request.data
+        serializer = ProjectDetailSerializer(
+            instance=project,
+            data=data,
+            partial=True
+        )
+        if serializer.is_valid():
+            serializer.save()
 
 
 
